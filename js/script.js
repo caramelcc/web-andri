a6ba400bfdbc87721d65b8121a3b29899375ad8a
caramelcc <108452543+caramelcc@users.noreply.github.com>
2022-07-28 12:22:43 +0700

    first commit portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// function
+function check() {
+
+    // getting data
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let msg = document.getElementById("textarea");
+
+    // alert
+    alert("nama : " + name.value + ", " + "email : " + email.value + ", " + "Message : " + msg.value);
+
+}
